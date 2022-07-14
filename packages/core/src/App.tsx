6f534d390440d8f471c976397e3db8ae6ecde110
michaelballos <ballos.michael@gmail.com>
2022-07-14 18:41:27 -0500

    Add authorization on button click

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const authorize = async () => {
+  await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.GITHUB_BEARER_TOKEN}`,
+    },
+    body: JSON.stringify({ query: 'query { viewer { login } }' }),
+  }).then((res) => res.json()).then((data) => console.log(data));
+};
+
 export default function App() {
   return (
-    <div>
-      aapp
-    </div>
+    <button onClick={authorize} type="button">
+      Status
+    </button>
   );
 }

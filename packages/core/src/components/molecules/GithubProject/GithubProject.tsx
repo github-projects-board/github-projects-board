0a1f06e56bfe701f73334196c3caa80b6368616b
michaelballos <ballos.michael@gmail.com>
2022-07-15 10:15:00 -0500

    Create auth component

--- packages/core/src/components/molecules/GithubProject/GithubProject.tsx
+++ packages/core/src/components/molecules/GithubProject/GithubProject.tsx
@@ -1,43 +1,16 @@
-import React, {
-  useEffect,
-  useState,
-} from 'react';
-
-type BearerToken = string | undefined;
+import React from 'react';
+import Authentication, {
+  BearerToken,
+} from '../../atoms/Authentication/Authentication';
 
 export interface IGithubProject {
   bearerToken: BearerToken;
 }
 
-const authorize = async (bearerToken: BearerToken) => {
-  const response = await fetch('https://api.github.com/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${bearerToken}`,
-    },
-    body: JSON.stringify({ query: 'query { viewer { login } }' }),
-  });
-  return response.json();
-};
-
 export default function GithubProject({ bearerToken }: IGithubProject) {
-  const [
-    isAuthorized,
-    setAuthorized,
-  ] = useState(false);
-  useEffect(() => {
-    if (!isAuthorized && bearerToken) {
-      authorize(bearerToken);
-      setAuthorized(true);
-    }
-  }, [
-    isAuthorized,
-    bearerToken,
-  ]);
   return (
     <div>
-      Project Board
+      <Authentication bearerToken={bearerToken} />
     </div>
   );
 }

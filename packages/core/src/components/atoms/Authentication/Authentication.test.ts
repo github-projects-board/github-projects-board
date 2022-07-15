c04e06a8884ec637417975c0b05b931e32108ba7
michaelballos <ballos.michael@gmail.com>
2022-07-15 12:41:46 -0500

    Add auth success tests

--- packages/core/src/components/atoms/Authentication/Authentication.test.ts
+++ packages/core/src/components/atoms/Authentication/Authentication.test.ts
@@ -1,7 +1,24 @@
 import { authorize } from './Authentication';
 
+const MOCK_BEARER_TOKEN = 'Bearer 12345';
+const MOCK_RESPONSE = {
+  data: {
+    views: {
+      login: 'elephantpenis',
+    },
+  },
+};
+
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve(MOCK_RESPONSE),
+})) as jest.Mock;
+
 describe('Authentication', () => {
   it('should be defined', () => {
     expect(authorize).toBeDefined();
+  });
+  it('should return a user after authentication', async () => {
+    const response = await authorize(MOCK_BEARER_TOKEN);
+    expect(response).toEqual(MOCK_RESPONSE);
   });
 });
